1db70aaaf4c87ffd50aafcad17bc4ac1cac8783b
Yasin Korkmaz <theacherx@gmail.com>
2023-10-22 19:30:30 +0300

    HasMany Relationship withCount

--- app/Filament/Resources/VoucherResource.php
+++ app/Filament/Resources/VoucherResource.php
@@ -40,6 +40,9 @@ class VoucherResource extends Resource
                 Tables\Columns\TextColumn::make('code'),
                 Tables\Columns\TextColumn::make('discount_percent')->label('Discount (%)'),
                 Tables\Columns\TextColumn::make('product.name'),
+                Tables\Columns\TextColumn::make('payments_count')
+                    ->counts('payments')
+                    ->label('Times Used'),
             ])
             ->filters([
                 //
