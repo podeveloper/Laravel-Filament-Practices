f6c87b68f1b58d7260f6e50a05ccc158e03d008e
Yasin Korkmaz <theacherx@gmail.com>
2023-10-22 20:06:36 +0300

    Adding Custom Actions To The Table

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -5,14 +5,19 @@ namespace App\Filament\Resources;
 use App\Filament\Resources\UserResource\Pages;
 use App\Filament\Resources\UserResource\RelationManagers;
 use App\Models\User;
+use Filament\Actions\Action;
 use Filament\Forms;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
+use Filament\Notifications\Notification;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules\Password;
 
 class UserResource extends Resource
 {
@@ -45,6 +50,30 @@ class UserResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\Action::make('changePassword')
+                    ->form([
+                        TextInput::make('new_password')
+                            ->password()
+                            ->label('New Password')
+                            ->required()
+                            ->rule(Password::default()),
+                        TextInput::make('new_password_confirmation')
+                            ->password()
+                            ->label('Confirm New Password')
+                            ->required()
+                            ->same('new_password')
+                            ->rule(Password::default()),
+                    ])
+                    ->action(function (User $record, array $data){
+                        $record->update([
+                            'password' => Hash::make($data['new_password'])
+                        ]);
+
+                        Notification::make()
+                            ->title('Password updated successfully')
+                            ->success()
+                            ->send();
+                    })
             ])
             ->bulkActions([
                 //
