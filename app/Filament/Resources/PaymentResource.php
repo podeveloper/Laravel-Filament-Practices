72e9fa1aba5a3dcbe2ff9fa930838066187a6539
Yasin Korkmaz <theacherx@gmail.com>
2023-10-22 20:14:23 +0300

    Payment Resource

---
+++ app/Filament/Resources/PaymentResource.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PaymentResource\Pages;
+use App\Filament\Resources\PaymentResource\RelationManagers;
+use App\Models\Payment;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class PaymentResource extends Resource
+{
+    protected static ?string $model = Payment::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('created_at')->label('Payment Time'),
+                Tables\Columns\TextColumn::make('product.name')->label('Product Name'),
+                Tables\Columns\TextColumn::make('user.name')->label('User Name'),
+                Tables\Columns\TextColumn::make('user.email')->label('User Email'),
+                Tables\Columns\TextColumn::make('voucher.code')->label('Voucher Code'),
+                Tables\Columns\TextColumn::make('subtotal')->money('usd'),
+                Tables\Columns\TextColumn::make('taxes')->money('usd'),
+                Tables\Columns\TextColumn::make('total')->money('usd'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                //
+            ])
+            ->bulkActions([
+                //
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPayments::route('/'),
+        ];
+    }
+
+    public static function canCreate() : bool
+    {
+        return false;
+    }
+
+    public static function canDeleteAny() : bool
+    {
+        return false;
+    }
+}
--- app/Policies/PaymentPolicy.php
+++ app/Policies/PaymentPolicy.php
@@ -13,7 +13,7 @@ class PaymentPolicy
      */
     public function viewAny(User $user): bool
     {
-        //
+        return true;
     }
 
     /**
@@ -21,7 +21,7 @@ class PaymentPolicy
      */
     public function view(User $user, Payment $payment): bool
     {
-        //
+        return true;
     }
 
     /**
@@ -29,7 +29,7 @@ class PaymentPolicy
      */
     public function create(User $user): bool
     {
-        //
+        return true;
     }
 
     /**
@@ -37,7 +37,7 @@ class PaymentPolicy
      */
     public function update(User $user, Payment $payment): bool
     {
-        //
+        return true;
     }
 
     /**
@@ -45,7 +45,7 @@ class PaymentPolicy
      */
     public function delete(User $user, Payment $payment): bool
     {
-        //
+        return true;
     }
 
     /**
@@ -53,7 +53,7 @@ class PaymentPolicy
      */
     public function restore(User $user, Payment $payment): bool
     {
-        //
+        return true;
     }
 
     /**
@@ -61,6 +61,6 @@ class PaymentPolicy
      */
     public function forceDelete(User $user, Payment $payment): bool
     {
-        //
+        return true;
     }
 }
