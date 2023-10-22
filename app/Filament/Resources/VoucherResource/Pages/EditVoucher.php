54b3032a00cf67d0e89021df25a7b26482b8ac96
Yasin Korkmaz <theacherx@gmail.com>
2023-10-22 19:42:22 +0300

    Restricting Edit on Some Condition & Triggering  Notification

--- app/Filament/Resources/VoucherResource/Pages/EditVoucher.php
+++ app/Filament/Resources/VoucherResource/Pages/EditVoucher.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources\VoucherResource\Pages;
 
 use App\Filament\Resources\VoucherResource;
 use Filament\Actions;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\EditRecord;
 
 class EditVoucher extends EditRecord
@@ -15,5 +16,18 @@ class EditVoucher extends EditRecord
         return [
             Actions\DeleteAction::make(),
         ];
+    }
+
+    protected function beforeFill()
+    {
+        if ($this->record->payments()->exists()) {
+
+            Notification::make()
+                ->title('You can not edit the voucher after it has been used')
+                ->danger()
+                ->send();
+
+            $this->redirect($this->getResource()::getUrl('index'));
+        }
     }
 }
