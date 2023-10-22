c743bd4952933c1d9ff9d4419fa3af18261c5fc8
Yasin Korkmaz <theacherx@gmail.com>
2023-10-22 23:17:41 +0300

    Addin Edit Urls on Table

--- app/Filament/Resources/PaymentResource.php
+++ app/Filament/Resources/PaymentResource.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources;
 use App\Filament\Resources\PaymentResource\Pages;
 use App\Filament\Resources\PaymentResource\RelationManagers;
 use App\Models\Payment;
+use App\Models\User;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
@@ -32,8 +33,10 @@ class PaymentResource extends Resource
         return $table
             ->columns([
                 Tables\Columns\TextColumn::make('created_at')->label('Payment Time')->sortable(),
-                Tables\Columns\TextColumn::make('product.name')->label('Product Name'),
-                Tables\Columns\TextColumn::make('user.name')->label('User Name'),
+                Tables\Columns\TextColumn::make('product.name')->label('Product Name')
+                    ->url(fn(Payment $record) => ProductResource::getUrl('edit', ['record' => $record->product])),
+                Tables\Columns\TextColumn::make('user.name')->label('User Name')
+                    ->url(fn(Payment $record) => UserResource::getUrl('edit', ['record' => $record->user])),
                 Tables\Columns\TextColumn::make('user.email')->label('User Email'),
                 Tables\Columns\TextColumn::make('voucher.code')->label('Voucher Code'),
                 Tables\Columns\TextColumn::make('subtotal')->money('usd'),
