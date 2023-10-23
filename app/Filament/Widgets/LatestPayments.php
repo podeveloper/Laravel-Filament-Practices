5b649a0f63f6b5e878761eb26f1fe396ed4942f4
Yasin Korkmaz <theacherx@gmail.com>
2023-10-23 07:29:16 +0300

    Table Widget

---
+++ app/Filament/Widgets/LatestPayments.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Payment;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Filament\Widgets\TableWidget as BaseWidget;
+
+class LatestPayments extends BaseWidget
+{
+    public function table(Table $table): Table
+    {
+        return $table
+            ->query(
+                Payment::with('product')->latest()->take(5)
+            )
+            ->columns([
+                Tables\Columns\TextColumn::make('created_at')->label('Time'),
+                Tables\Columns\TextColumn::make('total')->money(),
+                Tables\Columns\TextColumn::make('product.name'),
+            ]);
+    }
+
+    protected function isTablePaginationEnabled(): bool
+    {
+        return false;
+    }
+}
